ee0907d44f9f270cc8f908da06a82b835f6ab22c
Bruno Scigliano <bruno1457@gmail.com>
2017-10-28 20:14:57 -0300

    Agregado cargados de archivos

--- app/views.py
+++ app/views.py
@@ -9,6 +9,9 @@ from django_filters.views import FilterView
 from django_tables2.views import SingleTableMixin
 from app.omnibus import get_horarios, load_nodos, busqueda
 import csv
+from io import StringIO
+
+from django.core.files.storage import default_storage
 
 global shapeAuto
 global shapeCaminando
@@ -22,56 +25,10 @@ sf = shapefile.Reader('app/shpWkng.shp')
 shapeCaminando = sf.shapes()
 
 def index(request):
-    #Individuo.objects.all().delete()
-    #Sector.objects.all().delete()
-    #AnclaTemporal.objects.all().delete()
-    #Pediatra.objects.all().delete()
-    #Centro.objects.all().delete()
-    #TipoTransporte.objects.all().delete()
-    #Prestador.objects.all().delete()
-    #SectorTiempo.objects.all().delete()
-    #p = Prestador(11,"Medica Uruguaya")
-    #p.save()
-    #p = Prestador(2,"Asociacion Española")
-    #p.save()
-    #t =TipoTransporte(1,"Auto")
-    #t.save()
-    #t = TipoTransporte(0,"Caminando")
-    #t.save()
-    #t = TipoTransporte(2,"bus")
-    #t.save()
-    #t = TipoTransporte(3,"bus")
-    #t.save()
-    #p = Prestador(1,"ASSE")
-    #p.save()
-    #p = Prestador(3,"CASA DE GALICIA")
-    #p.save()
-    #p = Prestador(4,"CASMU")
-    #p.save()
-    #p = Prestador(5,"CIRCULO CATOLICO")
-    #p.save()
-    #p = Prestador(6,"COSEM")
-    #p.save()
-    #p = Prestador(7,"CUDAM")
-    #p.save()
-    #p = Prestador(8,"GREMCA")
-    #p.save()
-    #p = Prestador(9,"HOSPITAL BRITANICO")
-    #p.save()
-    #p = Prestador(10,"HOSPITAL EVANGELICO")
-    #p.save()
-    #p = Prestador(12,"MP")
-    #p.save()
-    #p = Prestador(13,"SMI")
-    #p.save()
-    #p = Prestador(14,"UNIVERSAL")
-    #p.save()
-    #p = Prestador(15,"NOEXISTE")
-    #p.save()
-    #cargarSectores()
-    #cargarCentroPediatras()
-    #cargarIndividuoAnclas()
-    #cargarTiempos()
+
+    ##cargarCentroPediatras()
+    ##cargarIndividuoAnclas()
+    ##cargarTiempos()
     post = request.POST
     if(not Settings.objects.filter(setting = "tiempoMaximo")):
         s = Settings(setting = "tiempoMaximo",value = "60")
@@ -82,6 +39,19 @@ def index(request):
     if(post):
         tiempoMax = post.get("tiempoTransporte")
         tiempoCons = post.get("tiempoConsulta")
+        radioCargado = post.get("optionsRadios")
+        radioMatrix =  radioCargado
+        if(radioCargado):
+            if(radioCargado == "option1"):
+                cargarMutualistas(request)
+            elif(radioCargado == "option2"):
+                cargarIndividuoAnclas(request)
+            elif(radioMatrix == "option3"):
+                cargarTiempos(0,request)
+            elif(radioMatrix == "option4"):
+                cargarTiempos(1,request)
+            elif(radioMatrix == "option5"):
+                cargarCentroPediatras(request)
         if(tiempoMax):
             maxT = Settings.objects.get(setting = "tiempoMaximo")
             maxT.value = tiempoMax
@@ -98,11 +68,11 @@ def res(request):
     print("********************************************************************************************************************************************************")
     isJardin = request.POST.get("anclaJar")
     isTrabajo = request.POST.get("anclaTra")
-    if (request.POST.get("checkB") == 'default'):
+    if request.POST.get("checkB") == 'default':
         pr = request.POST.get("checkB")
     else:
         pr = request.POST.get("mutualistasRadio")
-    tiempoMaximo = int(Settings.objects.get(setting = "tiempoMaximo").value) #Cambiar(Tomar de bd)
+    tiempoMaximo = int(Settings.objects.get(setting = "tiempoMaximo").value)  # Cambiar(Tomar de bd)
     tiempoConsulta = int(Settings.objects.get(setting = "tiempoConsulta").value) #Cambiar(Tomar de bd)
     transporte = request.POST.get("checkM") if request.POST.get("checkM") else request.POST.get("transporteRadio")
     individuos = Individuo.objects.all()
@@ -180,6 +150,46 @@ def res(request):
     context = {'result': table, 'dias':dias}
     return render(request, 'app/calcAll2.html', context)
     #return render(request, 'app/res.html')
+def cargarMutualistas(archivo):
+        Prestador.objects.all().delete()
+        p = Prestador(11,"Medica Uruguaya")
+        p.save()
+        p = Prestador(2,"Asociacion Española")
+        p.save()
+        t =TipoTransporte(1,"Auto")
+        t.save()
+        t = TipoTransporte(0,"Caminando")
+        t.save()
+        t = TipoTransporte(2,"bus")
+        t.save()
+        t = TipoTransporte(3,"bus")
+        t.save()
+        p = Prestador(1,"ASSE")
+        p.save()
+        p = Prestador(3,"CASA DE GALICIA")
+        p.save()
+        p = Prestador(4,"CASMU")
+        p.save()
+        p = Prestador(5,"CIRCULO CATOLICO")
+        p.save()
+        p = Prestador(6,"COSEM")
+        p.save()
+        p = Prestador(7,"CUDAM")
+        p.save()
+        p = Prestador(8,"GREMCA")
+        p.save()
+        p = Prestador(9,"HOSPITAL BRITANICO")
+        p.save()
+        p = Prestador(10,"HOSPITAL EVANGELICO")
+        p.save()
+        p = Prestador(12,"MP")
+        p.save()
+        p = Prestador(13,"SMI")
+        p.save()
+        p = Prestador(14,"UNIVERSAL")
+        p.save()
+        p = Prestador(15,"NOEXISTE")
+        p.save()
 
 def getPrestador(request,prestadores,individuo,pr):
     if (pr == 'default'):
@@ -217,6 +227,13 @@ def getSector(lugar, transporte):
             return lugar.sector_caminando
     else:
         return lugar
+def guardarArchivo(nombre, archivo):
+    with default_storage.open('tmp/'+nombre, 'wb+') as destination:
+        for chunk in archivo.chunks():
+            destination.write(chunk)
+        csv = os.path.join(settings.MEDIA_ROOT, destination)
+        return csv
+
 
 def cargarSectores():
     for i in range(len(shapeAuto)):
@@ -241,36 +258,38 @@ def parsear_hora(hora):
     h,m = hora.split('.')
     return int(h.zfill(2) + m.zfill(2))
 
-def cargarIndividuoAnclas():
-    with open('app/Ejemplo datos 5oct.csv', newline='') as csvfile:
-        l = csv.reader(csvfile, delimiter=',', quotechar='"')
-        lineas=[]
-        lineas.extend(l)
-        lineas = lineas[1:]
-        idAncla = 0
-        for caso in lineas:
-            ## Ancla
-            #Coordenada X, Coordenada Y, Tipo, Hora inicio, Hora fin, Dias, Sector auto, Sector caminando
-            #Duda Tecnica -Contemplar casos donde no hay jardin y/o trabajo
-            anclaJardin  = AnclaTemporal(idAncla,float(caso[10]),float(caso[11]),"jardin" ,parsear_hora(caso[7]) ,parsear_hora(caso[8]) ,caso[6] ,None,None)
-            anclaJardin.sector_auto = getSectorForPoint(anclaJardin,"Auto")
-            anclaJardin.sector_caminando = getSectorForPoint(anclaJardin,"Caminando")
-            anclaJardin.save()
-            idAncla +=1
-            anclaTrabajo = AnclaTemporal(idAncla,float(caso[14]),float(caso[15]),"trabajo",parsear_hora(caso[17]),parsear_hora(caso[18]),caso[16],None,None)
-            anclaTrabajo.sector_auto = getSectorForPoint(anclaTrabajo,"Auto")
-            anclaTrabajo.sector_caminando = getSectorForPoint(anclaTrabajo,"Caminando")
-            anclaTrabajo.save()
-            idAncla +=1
-            anclaHogar   = AnclaTemporal(idAncla,float(caso[22]),float(caso[23]),"hogar",None,None,"L-D",None,None)
-            anclaHogar.sector_auto = getSectorForPoint(anclaHogar,"Auto")
-            anclaHogar.sector_caminando = getSectorForPoint(anclaHogar,"Caminando")
-            anclaHogar.save()
-            idAncla +=1
-            ## Individuo
-            #Id, Tipo transporte, Prestador, Hogar, Trabajo, Jardin
-            individuo  = Individuo(int(caso[0]),int(caso[19]),int(caso[1]),anclaHogar.id,anclaTrabajo.id,anclaJardin.id)
-            individuo.save()
+def cargarIndividuoAnclas(requestf):
+    Individuo.objects.all().delete()
+    csvfile = requestf.FILES['inputFile']
+    csvf = StringIO(csvfile.read().decode())
+    l = csv.reader(csvf, delimiter=',', quotechar='"')
+    lineas=[]
+    lineas.extend(l)
+    lineas = lineas[1:]
+    idAncla = 0
+    for caso in lineas:
+        ## Ancla
+        #Coordenada X, Coordenada Y, Tipo, Hora inicio, Hora fin, Dias, Sector auto, Sector caminando
+        #Duda Tecnica -Contemplar casos donde no hay jardin y/o trabajo
+        anclaJardin  = AnclaTemporal(idAncla,float(caso[10]),float(caso[11]),"jardin" ,parsear_hora(caso[7]) ,parsear_hora(caso[8]) ,caso[6] ,None,None)
+        anclaJardin.sector_auto = getSectorForPoint(anclaJardin,"Auto")
+        anclaJardin.sector_caminando = getSectorForPoint(anclaJardin,"Caminando")
+        anclaJardin.save()
+        idAncla +=1
+        anclaTrabajo = AnclaTemporal(idAncla,float(caso[14]),float(caso[15]),"trabajo",parsear_hora(caso[17]),parsear_hora(caso[18]),caso[16],None,None)
+        anclaTrabajo.sector_auto = getSectorForPoint(anclaTrabajo,"Auto")
+        anclaTrabajo.sector_caminando = getSectorForPoint(anclaTrabajo,"Caminando")
+        anclaTrabajo.save()
+        idAncla +=1
+        anclaHogar   = AnclaTemporal(idAncla,float(caso[22]),float(caso[23]),"hogar",None,None,"L-D",None,None)
+        anclaHogar.sector_auto = getSectorForPoint(anclaHogar,"Auto")
+        anclaHogar.sector_caminando = getSectorForPoint(anclaHogar,"Caminando")
+        anclaHogar.save()
+        idAncla +=1
+        ## Individuo
+        #Id, Tipo transporte, Prestador, Hogar, Trabajo, Jardin
+        individuo  = Individuo(int(caso[0]),int(caso[19]),int(caso[1]),anclaHogar.id,anclaTrabajo.id,anclaJardin.id)
+        individuo.save()
 
 def getSectorForPoint(ancal,tipo):
     if(tipo == "Auto"):
@@ -283,58 +302,68 @@ def getSectorForPoint(ancal,tipo):
         if(polygon.contains(point)):
             return Sector.objects.get(shape = i, tipo_sector = tipo)
 
-def cargarTiempos():
-    with open('app/autoTiempos.csv', newline='') as csvfile:
-        l = csv.reader(csvfile, delimiter=',')
-        lineas=[]
-        lineas.extend(l)
-        lineas = lineas[1:]
-        id = 0
-        tiempos = []
-        #sectores = Sector.objects.all()
-        for caso in lineas:
-            tiempo = SectorTiempo(id = id , sector_1_id = int(caso[0]), sector_2_id = int(caso[1]),tiempo = float(caso[2]), distancia = float(caso[3]))
-            tiempos.append(tiempo)
-            id +=1
-            if(id % 100000 == 0):
-                guardar = SectorTiempo.objects.bulk_create(tiempos)
-                tiempos = []
-        if(tiempos):
+def cargarTiempos(tipo,request):
+    print(request.FILES)
+    if(tipo == 0):
+        SectorTiempo.objects.filter(id__lt = len(shapeAuto)).delete()
+    else:
+        SectorTiempo.objects.filter(id__gte = len(shapeAuto)).delete()
+    csvfile = request.FILES['inputFile']
+    csvf = StringIO(csvfile.read().decode())
+    l = csv.reader(csvf, delimiter=',')
+    lineas=[]
+    lineas.extend(l)
+    lineas = lineas[1:]
+    id = 0
+    tiempos = []
+    #sectores = Sector.objects.all()
+    for caso in lineas:
+        tiempo = SectorTiempo(id = id , sector_1_id = int(caso[0]), sector_2_id = int(caso[1]),tiempo = float(caso[2]), distancia = float(caso[3]))
+        tiempos.append(tiempo)
+        id +=1
+        if(id % 100000 == 0):
             guardar = SectorTiempo.objects.bulk_create(tiempos)
+            tiempos = []
+    if(tiempos):
+        guardar = SectorTiempo.objects.bulk_create(tiempos)
 
-def cargarCentroPediatras():
-    with open('app/centros.csv', newline='') as csvfile:
-        l = csv.reader(csvfile, delimiter=',', quotechar='"')
-        lineas=[]
-        lineas.extend(l)
-        lineas = lineas[1:]
-        inti = 0
-        horas = ["6.0","7.0","8.0","9.0","10.0","11.0","12.0","13.0","14.0","15.0","16.0","17.0","18.0","19.0","20.0","21.0"]
-        id = 0
-        for caso in lineas:
-        ## Centro
-        #Id, Coordenada X, Coordenada Y, Sector, Direccion, Prestador
-            if(caso[2]!=""):
-                #int(caso[3] id para cuando sean unicas
-                centro = Centro(inti,float(caso[9]),float(caso[10]),None,None,caso[7],int(caso[2]))
-                centro.sector_auto = getSectorForPoint(centro,"Auto")
-                centro.sector_caminando = getSectorForPoint(centro,"Caminando")
-                centro.save()
-            ## Pediatra
-            #Centro, Dia, Hora, Cantidad de pediatras
-            #dias = ["LUNES","MARTES","MIERCOLES","JUEVES","VIERNES","SABADO"]
-                contador_dias = 11
-                for i in range(5):
-                    for j in horas:
-                        if(contador_dias >106): ##Arreglar
-                            break
-                        if ((caso[contador_dias]) == ''):
-                            caso[contador_dias]=0
-                            pediatra = Pediatra (centro_id = inti,dia = i, hora = parsear_hora(j), cantidad_pediatras = int(caso[contador_dias]))
-                            pediatra.save()
-                            contador_dias +=1
-                        else:
-                            pediatra = Pediatra (centro_id = inti,dia = i, hora = parsear_hora(j), cantidad_pediatras = int(caso[contador_dias]))
-                            contador_dias +=1
-                            pediatra.save()
-                inti +=1
+def cargarCentroPediatras(request):
+    Pediatra.objects.all().delete()
+    csvfile = request.FILES['inputFile']
+    csvf = StringIO(csvfile.read().decode())
+    l = csv.reader(csvf, delimiter=',')
+    lineas=[]
+    lineas.extend(l)
+    lineas = lineas[1:]
+    inti = 0
+    horas = ["6.0","7.0","8.0","9.0","10.0","11.0","12.0","13.0","14.0","15.0","16.0","17.0","18.0","19.0","20.0","21.0"]
+    id = 0
+    for caso in lineas:
+    ## Centro
+    #Id, Coordenada X, Coordenada Y, Sector, Direccion, Prestador
+        if(caso[2]!=""):
+            #int(caso[3] id para cuando sean unicas
+            centro = Centro(inti,float(caso[9]),float(caso[10]),None,None,caso[7],int(caso[2]))
+            centro.sector_auto = getSectorForPoint(centro,"Auto")
+            centro.sector_caminando = getSectorForPoint(centro,"Caminando")
+            centro.save()
+        ## Pediatra
+        #Centro, Dia, Hora, Cantidad de pediatras
+        #dias = ["LUNES","MARTES","MIERCOLES","JUEVES","VIERNES","SABADO"]
+            contador_dias = 11
+            for i in range(5):
+                for j in horas:
+                    if(contador_dias >106): ##Arreglar
+                        break
+                    if ((caso[contador_dias]) == ''):
+                        caso[contador_dias]=0
+                        pediatra = Pediatra (centro_id = inti,dia = i, hora = parsear_hora(j), cantidad_pediatras = int(caso[contador_dias]))
+                        pediatra.save()
+                        contador_dias +=1
+                    else:
+                        pediatra = Pediatra (centro_id = inti,dia = i, hora = parsear_hora(j), cantidad_pediatras = int(caso[contador_dias]))
+                        contador_dias +=1
+                        pediatra.save()
+            inti +=1
+if(not Sector.objects.all()):
+    cargarSectores()
