f1a76874886a7574a19f16762749c2a2bd5ac65c
german-rodriguez <german.rodriguez@correo.ucu.edu.uy>
2018-01-31 20:29:21 -0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,7 +1,7 @@
 @echo off
 start redis-latest/redis-server
 start elcelery.bat
-git pull origin prod
+git pull origin prod -f
 python manage.py makemigrations app
 python manage.py migrate
 python manage.py runserver
