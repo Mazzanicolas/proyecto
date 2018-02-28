67c5a2763a17b51b60367c67fc68bb143e9994b5
german-rodriguez <german.rodriguez@correo.ucu.edu.uy>
2018-02-28 20:53:48 -0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,1 +1,5 @@
-python manage.py runserver
+set FORKED_BY_MULTIPROCESSING=1
+start redis-latest/redis-server
+start Celery -A proyecto worker -Q CalculationQueue --max-memory-per-child=150000 -E 
+start Celery -A proyecto worker -Q delegate --max-tasks-per-child=1 -E -n Worker2 
+python manage.py runserver --insecure
